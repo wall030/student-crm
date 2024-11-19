3974e188ffc085afa373d5a007e82737459668dc
Jan Wall <jwa@novatec-gmbh.de>
2024-11-19 11:55:19 +0100

    refactor: studentservice integration test arrange testing data once

--- backend/src/test/kotlin/org/acme/service/StudentServiceTest.kt
+++ backend/src/test/kotlin/org/acme/service/StudentServiceTest.kt
@@ -28,43 +28,37 @@ class StudentServiceTest {
     @Inject
     private lateinit var studentService: StudentService
 
+    private val student1 = StudentEntity(1L, "Luke", "Skywalker", "luke@jedi.com")
+    private val student2 = StudentEntity(2L, "Han", "Solo", "solo@smuggler.com")
+    private val studentsList = listOf(student1, student2)
+    private val studentIDs = listOf(student1.id, student2.id)
+    private val studentDTO = StudentCreateUpdateDTO("Darth", "Vader", "vader@sith.com")
+
+    private val course1 = CourseEntity(1L, "Piloting 101")
+    private val course2 = CourseEntity(2L, "Lightsaber Combat")
+    private val coursesList = listOf(course1, course2)
+    private val courseIDs = listOf<Long>(course1.id, course2.id)
+
     @Test
     fun `should list all students`() {
-        val students =
-            mutableListOf<StudentEntity>(
-                StudentEntity("Luke", "Skywalker", "luke@jedi.com"),
-                StudentEntity("Leia", "Organa", "leia@rebel.com"),
-            )
-
-        every { studentRepository.listAll() } returns students
+        every { studentRepository.listAll() } returns studentsList
         val result = studentService.findAllStudents()
-        expectThat(result).isEqualTo(students.map { student -> student.toStudentDTO() })
+        expectThat(result).isEqualTo(studentsList.map { student -> student.toStudentDTO() })
     }
 
     @Test
     fun `find student by id`() {
-        val student = StudentEntity(1L, "Han", "Solo", "solo@smuggler.com")
-
-        every { studentRepository.findById(student.id) } returns student
-        val result = studentService.findStudent(student.id)
-        expectThat(result).isEqualTo(student.toStudentDTO())
+        every { studentRepository.findById(student1.id) } returns student1
+        val result = studentService.findStudent(student1.id)
+        expectThat(result).isEqualTo(student1.toStudentDTO())
     }
 
     @Test
     @Transactional
     fun `create Student`() {
-        val studentDTO = StudentCreateUpdateDTO("Darth", "Vader", "vader@sith.com")
-
-        every {
-            studentRepository.persist(any<StudentEntity>())
-        } returns Unit
-
-        every {
-            studentRepository.findByEmail(studentDTO.email)
-        } returns null
-
+        every { studentRepository.persist(any<StudentEntity>()) } returns Unit
+        every { studentRepository.findByEmail(studentDTO.email) } returns null
         val result = studentService.createStudent(studentDTO.firstName, studentDTO.lastName, studentDTO.email)
-
         expectThat(result)
             .and {
                 get { firstName }.isEqualTo(studentDTO.firstName)
@@ -80,13 +74,10 @@ class StudentServiceTest {
     @Test
     @Transactional
     fun `should update student attributes`() {
-        val student = StudentEntity(1L, "Darth", "Maul", "noleg@sith.com")
-        val studentDTO = student.toStudentDTO()
-
-        every { studentRepository.findById(studentDTO.id) } returns student
-        every { studentRepository.findByEmail(studentDTO.email) } returns student
-        every { studentRepository.persist(student) } returns Unit
-
+        val studentDTO = student1.toStudentDTO()
+        every { studentRepository.findById(studentDTO.id) } returns student1
+        every { studentRepository.findByEmail(studentDTO.email) } returns student1
+        every { studentRepository.persist(student1) } returns Unit
         val result =
             studentService.updateStudent(
                 studentDTO.id,
@@ -100,41 +91,19 @@ class StudentServiceTest {
     @Test
     @Transactional
     fun `should delete a list of students`() {
-        val studentIDs = listOf(1L, 2L)
-        val students =
-            listOf(
-                StudentEntity(1L, "Darth", "Maul", "noleg@sith.com"),
-                StudentEntity(2L, "Han", "Solo", "solo@smuggler.com"),
-            )
-
-        every { studentRepository.findByIds(studentIDs) } returns students
-        every { studentRepository.deleteByIds(studentIDs) } returns 2
+        every { studentRepository.findByIds(studentIDs) } returns studentsList
+        every { studentRepository.deleteByIds(studentIDs) } returns studentIDs.size.toLong()
         val result = studentService.deleteStudents(studentIDs)
-        verify { studentRepository.deleteByIds(studentIDs) }
         expectThat(result).isTrue()
     }
 
     @Test
     @Transactional
     fun `should assign a list of courses to a student`() {
-        val courseIDs = listOf<Long>(1L, 2L)
-        val studentID = 1L
-        val updatedStudent = StudentEntity(studentID, "Rey", "Palpatine", "rey@scavenger.com")
-        val course1 = CourseEntity(1L, "Piloting 101")
-        val course2 = CourseEntity(2L, "Lightsaber Combat")
-
-        every { courseRepository.findByIds(courseIDs) } returns listOf(course1, course2)
-        every { studentRepository.findById(studentID) } returns updatedStudent
-        every { studentRepository.persist(updatedStudent) } returns Unit
-
-        val result = studentService.assignCourses(studentID, courseIDs)
-
-        expectThat(result).isEqualTo(
-            listOf(
-                CourseDTO(course1.id, course1.name, emptyList()),
-                CourseDTO(course2.id, course2.name, emptyList()),
-            ),
-        )
-        verify { studentRepository.persist(updatedStudent) }
+        every { courseRepository.findByIds(courseIDs) } returns coursesList
+        every { studentRepository.findById(any()) } returns student1
+        every { studentRepository.persist(student1) } returns Unit
+        val result = studentService.assignCourses(student1.id, courseIDs)
+        expectThat(result).isEqualTo(listOf(course1.toCourseDTO(), course2.toCourseDTO()))
     }
 }
